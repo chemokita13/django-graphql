dc6ca817cba2a8ce9aab98dc066fe83bace2de08
José María Pahino <chemokita13@gmail.com>
2023-09-25 18:08:14 +0200

    added env debug

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 # Use an official Python runtime as a parent image
-FROM python:3.11.5-slim-buster
+FROM python:3.11.5-slim-bookworm
 
 # Set environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+ENV DJANGO_DEBUG=False
 
 # Set the working directory to /app
 WORKDIR /app
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b_$!m2cdkc__og=xwwp-ab%#bvuu=fq+as%8^!w5un*)@bxw%="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
